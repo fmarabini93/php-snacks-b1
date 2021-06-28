6e1cd2c8c7a3cefc247b24440e8d35b103aa43e1
fmarabini93 <fmarabini@icloud.com>
2021-06-28 15:26:49 +0200

    snack3

--- snack3/index.php
+++ snack3/index.php
@@ -1,3 +1,12 @@
 <?php
-
+    $numbers = [];
+    while (count($numbers)<15) {
+        $newNumber = rand(1,100);
+        if (!in_array($newNumber,$numbers)) {
+            $numbers[] = $newNumber;
+        }
+    }
+    for ($i=0; $i<15; $i++) {
+        echo $numbers[$i] . "<br />";
+    }
 ?>
