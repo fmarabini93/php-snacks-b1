ed6ff4b26b4ce7b3846e21f525af737dd7854c26
fmarabini93 <fmarabini@icloud.com>
2021-06-28 15:09:32 +0200

    snack2

--- snack2/index.php
+++ snack2/index.php
@@ -1,3 +1,11 @@
 <?php
+    $name = $_GET["n"];
+    $mail = $_GET["m"];
+    $age = $_GET["a"];
 
+    if (trim(strlen($name) > 3) && str_contains($mail,".") && str_contains($mail,"@") && is_int(intval($age))) {
+        echo "Access granted";
+    } else {
+        echo "Access forbidden";
+    }
 ?>
